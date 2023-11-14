e42e9285ee6c56a563de4fb0a8df7cd27356ab4a
Sadh26 <sadhana.suresh@solitontech.com>
2023-11-14 13:21:21 +0530

    fix: implement tryParse, access scope

--- Assignment2/Print_Star_Pattern/ConsoleApp1/Program.cs
+++ Assignment2/Print_Star_Pattern/ConsoleApp1/Program.cs
@@ -3,32 +3,42 @@
 /// <summary>
 /// This will print the star pattern.
 /// </summary>
-class PrintStar{
+public class PrintStar{
 
     /// <summary>
     /// The Main method gets the number of rows from the user.
     /// </summary>
-    static void Main(){
+    public static void Main(){
         int userRows;
+        bool isUserRowsValid;
+        
+        //This will check if the user input rows are NUMERIC and from 0 to 10. If not will ask the user to enter a valid input.
         do{
             Console.WriteLine("Enter the number of rows:");
-            userRows = int.Parse(Console.ReadLine());
-            if(userRows <= 0 || userRows > 10){
-                Console.WriteLine("Please enter a value between 0 and 10");
-            }      
+            isUserRowsValid = int.TryParse(Console.ReadLine(), out userRows);
+
+            if(!isUserRowsValid){
+                Console.WriteLine("Please enter a numeric value");
+            } 
+            else if(userRows < 0 || userRows > 10){
+                Console.WriteLine("Please enter a value from 0 to 10");
+            }     
         }
-        while(userRows <= 0 || userRows > 10);
+        while(!isUserRowsValid || userRows < 0 || userRows > 10);
         PrintPattern(userRows);
     }
 
     /// <summary>
-    /// This method will print the stars.
+    /// This method will print the star pattern.
     /// </summary>
-    private static void PrintPattern(int userRows){
-        for(int rows=1; rows<=userRows; rows++){
-            for(int spaces=1; spaces<=(userRows-rows); spaces++){
+    private static void PrintPattern(int userRow){
+        //Loop for rows
+        for(int rows=1; rows<=userRow; rows++){
+            //Loop for spaces
+            for(int spaces=1; spaces<=(userRow-rows); spaces++){
                 Console.Write(" ");
             }
+            //Loop for stars
             for(int stars=1; stars<=rows; stars++){
                 Console.Write("* ");
             }
--- Assignment2/To_UpperCase/ConsoleApp1/Program.cs
+++ Assignment2/To_UpperCase/ConsoleApp1/Program.cs
@@ -3,15 +3,16 @@
 /// <summary>
 /// This will convert the text provided to Upper Case.
 /// </summary>
-class ToUpperCase{
+public class ToUpperCase{
 
     /// <summary>
     /// This Main method will get the user input and converts to Upper case.
     /// </summary>
-    static void Main(){
+    public static void Main(){
         Console.WriteLine("***** TO UPPER CASE *****");
         bool end = false;
 
+        // Until the user types STOP this loop will run and end = false else end = true.
         while(!end){
             Console.WriteLine("Enter a string:");
             string userInputString = Console.ReadLine();
